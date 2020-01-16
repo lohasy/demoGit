e760c7931252462bae7e01b0fbf24ff69fefdbc6
yumx <1184315279@qq.com>
2020-01-16 14:58:02 +0800

    22222

--- sping4-project-demo/src/com/cxgyl/test/Demo.java
+++ sping4-project-demo/src/com/cxgyl/test/Demo.java
@@ -2,6 +2,8 @@ package com.cxgyl.test;
 
 public class Demo {
     public static void main(String[] args) {
+        List list = new ArrayList();
+        list.add("yumx");
         System.out.println("i am a study man");
     }
 }
