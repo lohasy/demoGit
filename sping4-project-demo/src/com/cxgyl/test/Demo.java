f998d08e10a7b1528ff38e37626ceb31452f794e
yumx <1184315279@qq.com>
2020-01-16 09:28:24 +0800

    22222

--- sping4-project-demo/src/com/cxgyl/test/Demo.java
+++ sping4-project-demo/src/com/cxgyl/test/Demo.java
@@ -1,7 +1,11 @@
 package com.cxgyl.test;
 
+import com.cxgyl.test.util.TestUtils;
+
 public class Demo {
     public static void main(String[] args) {
+        TestUtils.print("项目开始了");
         System.out.println("i am a study man");
+        TestUtils.print("项目结束了");
     }
 }
---
+++ sping4-project-demo/src/com/cxgyl/test/util/TestUtils.java
@@ -0,0 +1,7 @@
+package com.cxgyl.test.util;
+
+public class TestUtils {
+    public static void print(String str){
+        System.out.println("======>"+str);
+    }
+}
